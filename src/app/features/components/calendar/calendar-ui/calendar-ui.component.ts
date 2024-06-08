8c998820b1b087cd2a3d77957e00b5b5f42fe0b1
Tengiz Mikhelidze <mixelidzet@gmail.com>
2024-06-09 03:15:45 +0400

    feature - calendar chevrons generic logics

--- src/app/features/components/calendar/calendar-ui/calendar-ui.component.ts
+++ src/app/features/components/calendar/calendar-ui/calendar-ui.component.ts
@@ -1,9 +1,11 @@
-import {Component, model, signal, WritableSignal} from '@angular/core';
+import {Component, computed, effect, Injector, model, OnInit, signal, WritableSignal} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FaIconComponent} from "@fortawesome/angular-fontawesome";
 import {faChevronLeft, faChevronRight} from "@fortawesome/free-solid-svg-icons";
 import {NumberToMonth} from "../utility";
 import {NumberToDateString} from "../utility/number-to-date-string.utility";
+import {forkJoin} from "rxjs";
+import {toObservable} from "@angular/core/rxjs-interop";
 
 @Component({
     selector: 'app-calendar-ui',
@@ -12,7 +14,7 @@ import {NumberToDateString} from "../utility/number-to-date-string.utility";
     templateUrl: './calendar-ui.component.html',
     styleUrl: './calendar-ui.component.scss',
 })
-export class CalendarUiComponent {
+export class CalendarUiComponent implements OnInit{
     readonly numberToMonth = NumberToMonth;
     readonly numberToDateString = NumberToDateString;
     readonly chevronLeft = faChevronLeft
@@ -21,6 +23,18 @@ export class CalendarUiComponent {
     day = signal<string | undefined>(undefined)
     month = signal<string | undefined>(undefined)
     year = signal<string | undefined>(undefined)
+    generatedUi = signal<number[]>([])
+
+    constructor(private injector: Injector) {}
+    ngOnInit() {
+        this.generateUi();
+    }
+
+    initializeSignals(): void {
+        effect(() => {
+            console.log(`The count is: ${this.day()}`);
+        }, {injector: this.injector});
+    }
 
     chevronHandler(signaler: WritableSignal<string | undefined>, chevronType: 'left' | 'right') {
         signaler.update(value => {
@@ -34,5 +48,32 @@ export class CalendarUiComponent {
         this.day.set(this.numberToDateString(new Date(today).getDate()))
         this.month.set(this.numberToDateString(new Date(today).getMonth() + 1))
         this.year.set(this.numberToDateString(new Date(today).getFullYear()))
+    }
+
+    generateUi() {
+        const datesArr: number[] = [];
+        const day = Number(this.day()) || this.todayDate().getDate();
+        const month = Number(this.month()) || this.todayDate().getMonth();
+        const year = Number(this.year()) || this.todayDate().getFullYear();
+
+        let lastDateOfMonth = new Date(year, month + 1, 0).getDate()
+        let lastDateOfPrevMonth = new Date(year, month, 0).getDate()
+        let numbersBeforeFirstDate = new Date(year, month, 1).getDay() - 1;
+        let numbersAfterLastDate = 7 - new Date(year, month + 1, 0).getDay();
+
+        for (let i = 1; i <= lastDateOfMonth; i++) {
+            datesArr.push(i)
+        }
+
+        for(let i = 0; i < numbersBeforeFirstDate; i++) {
+            datesArr.unshift(lastDateOfPrevMonth - i)
+        }
+
+        for(let i = 0; i < numbersAfterLastDate; i++) {
+            datesArr.push(i + 1)
+        }
+
+        this.generatedUi.set(datesArr);
+        console.log(datesArr)
     }
 }
